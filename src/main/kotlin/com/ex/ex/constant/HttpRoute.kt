2faa3ec8926652ecc53ce26c71cda04f0df6a963
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-01-30 13:17:56 +0700

    Create http route path.

---
+++ src/main/kotlin/com/ex/ex/constant/HttpRoute.kt
@@ -0,0 +1,14 @@
+package com.ex.ex.constant
+
+object HttpRoute {
+
+    // VERSION
+    const val V_1 = "/v1"
+
+    // ROOT
+    const val AUTHENTICATION = "/authentication"
+
+    // END-POINT
+    const val LOGIN = "/login"
+
+}
